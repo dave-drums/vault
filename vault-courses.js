ecc46371f1912293e17c2a36bb949098cf3146ed
DD <info@davedrums.com.au>
2025-12-25 14:09:40 +1100

    Update vault-courses.js

--- vault-courses.js
+++ vault-courses.js
@@ -17,7 +17,13 @@
     'gs2': { name: 'Groove Studies 2', pathway: 'groove', lessons: [] },
     'gs3': { name: 'Groove Studies 3', pathway: 'groove', lessons: [] },
     'gs4': { name: 'Groove Studies 4', pathway: 'groove', lessons: [] },
-    'fs1': { name: 'Fill Studies 1', pathway: 'fills', lessons: [] },
+    'fs1': { 
+       name: 'Fill Studies 1', 
+       pathway: 'fills', 
+       lessons: ['1.01', '1.02', '1.03', '1.04', '1.05', '1.06', '1.07', '1.08', 
+                '1.09', '1.10', '1.11', '1.12', '1.13', '1.14', '1.15', '1.16',
+                '1.17', '1.18', '1.19', '1.20', '1.21', '1.22', '1.23']
+    },
     'fs2': { name: 'Fill Studies 2', pathway: 'fills', lessons: [] },
     'fs3': { name: 'Fill Studies 3', pathway: 'fills', lessons: [] },
     'fs4': { name: 'Fill Studies 4', pathway: 'fills', lessons: [] },
