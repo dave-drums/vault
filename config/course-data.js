a8a3c79258843c870929b2062b7adf60af3bc98b
DD <info@davedrums.com.au>
2025-12-30 15:19:45 +1000

    Update course-data.js

--- config/course-data.js
+++ config/course-data.js
@@ -41,12 +41,14 @@
     'ks1': { name: 'Kick Studies', level: 'Single Pedal Course', pathway: 'kicks', lessons: [] },
     'ks2': { name: 'Kick Studies', level: 'Double Pedal Course', pathway: 'kicks', lessons: [] },
     'ks3': { name: 'Kick Studies', level: 'Technique & Pedal Settings Course', pathway: 'kicks', lessons: [] },
-    'rr1': { 
-        name: 'Reading Rhythms', 
-        level: 'Beginner', 
+    'rs1': { 
+        name: 'Rhythm Studies', 
+        level: 'The Basics', 
         pathway: 'reading', 
         lessons: ['1.01', '1.02', '1.03', '1.04', '1.05', '1.06', '1.07', '1.08', '1.09', '1.10', '1.11', '1.12', '1.13', '1.14', '1.15', '1.16', '1.17', '1.18', '1.19', '1.20'] 
     },
+     'rs2': { name: 'Rhythm Studies', level: 'Triple Meters', pathway: 'reading', lessons: [] },
+     'rs3': { name: 'Rhythm Studies', level: 'Odd Meters', pathway: 'reading', lessons: [] },
   };
   
   // ===========================================
